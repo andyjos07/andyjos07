6d2370996d6520878ff05b1b8c8d337995d57ae0
andyjos07 <91872250+andyjos07@users.noreply.github.com>
2021-10-04 13:17:49 +0530

    Update youtube search automation.py

--- youtube search automation.py
+++ youtube search automation.py
@@ -2,14 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-song = 'dhoni'
+keyword = 'dhoni'
 
 driver = webdriver.Chrome ('C:\Program Files\chromedriver_win32\chromedriver.exe')
 driver.get ('https://www.youtube.com/')
 
 search_field=driver.find_element_by_name('search_query')
 print(search_field.is_displayed())
-search_field.send_keys(song)
+search_field.send_keys(keyword)
 time.sleep(3)
 search_field.send_keys(Keys.ENTER)
 
